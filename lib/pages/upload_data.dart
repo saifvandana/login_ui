c90023ad71d1c2c18d303bc5344b4033426201aa
saifvandana <vandanasaif@gmail.com>
2022-03-23 17:57:12 +0300

    added image upload

--- lib/pages/upload_data.dart
+++ lib/pages/upload_data.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers, prefer_final_fields, deprecated_member_use, unnecessary_import, prefer_const_declarations, dead_code
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers, prefer_final_fields, deprecated_member_use, unnecessary_import, prefer_const_declarations, dead_code, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,6 +8,7 @@ import 'package:markdown_editable_textinput/markdown_text_input.dart';
 import '../common/theme_helper.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:multi_image_picker2/multi_image_picker2.dart';
 
 class UploadData extends StatefulWidget {
   const UploadData({Key? key}) : super(key: key);
@@ -45,6 +46,61 @@ class _UploadDataState extends State<UploadData> {
   TextEditingController _address = TextEditingController();
   TextEditingController _email = TextEditingController();
 
+  List<Asset> images = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(images.length, (index) {
+        Asset asset = images[index];
+        return AssetThumb(
+          asset: asset,
+          width: 300,
+          height: 70,
+        );
+      }),
+    );
+  }
+
+  Future<void> loadAssets() async {
+    List<Asset> resultList = [];
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+        enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(
+          takePhotoIcon: "chat",
+          doneButtonTitle: "Fatto",
+        ),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#abcdef",
+          actionBarTitle: "Example App",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+        ),
+      );
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+    });
+  }
+
   String? selectedValue;
   List<String> items = [
     'Item1',
@@ -67,14 +123,13 @@ class _UploadDataState extends State<UploadData> {
         iconTheme: IconThemeData(color: Colors.white),
         flexibleSpace: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: <Color>[
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
                 Theme.of(context).primaryColor,
                 Theme.of(context).accentColor,
-              ]
-            )),
+              ])),
         ),
       ),
       body: SingleChildScrollView(
@@ -83,35 +138,40 @@ class _UploadDataState extends State<UploadData> {
           padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
           alignment: Alignment.center,
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               Form(
                 key: _formKey,
                 child: Column(
                   children: [
+                    Text('POST INFO'),
+                    SizedBox(
+                      height: 20,
+                    ),
                     Container(
                       child: DropdownButtonHideUnderline(
                         child: DropdownButton2(
                           hint: Padding(
                             padding: EdgeInsets.only(left: 10),
                             child: Text(
-                              'Category*',
+                              'Category *',
                               style: TextStyle(
-                                fontSize: 14,
+                                fontSize: 15,
                               ),
-                            ),),
+                            ),
+                          ),
                           iconEnabledColor: Theme.of(context).primaryColor,
                           items: items
-                                  .map((item) =>
-                                  DropdownMenuItem<String>(
+                              .map((item) => DropdownMenuItem<String>(
                                     value: item,
                                     child: Text(
                                       item,
                                       style: const TextStyle(
-                                        fontSize: 14,
+                                        fontSize: 15,
                                       ),
                                     ),
                                   ))
-                                  .toList(),
+                              .toList(),
                           value: selectedValue,
                           onChanged: (value) {
                             setState(() {
@@ -124,44 +184,103 @@ class _UploadDataState extends State<UploadData> {
                         ),
                       ),
                     ),
-                    SizedBox(height: 30,),
+                    SizedBox(
+                      height: 30,
+                    ),
                     Container(
                       child: TextFormField(
                         controller: _title,
                         decoration: ThemeHelper().textInputDecoration(
-                            'Title*'.tr, 'Enter the title of your post'.tr),
+                            'Title *'.tr, 'Enter the title of your post'.tr),
                       ),
                       //decoration: ThemeHelper().inputBoxDecorationShaddow(),
                     ),
-                    SizedBox(height: 30,),
+                    SizedBox(
+                      height: 30,
+                    ),
                     Container(
                       child: MarkdownTextInput(
-                          (String value) => setState(() => description = value),
-                          description,
-                          label: 'Description',
-                          actions: MarkdownType.values,
-                          controller: _about,
-                        ),
+                        (String value) => setState(() => description = value),
+                        description,
+                        label: 'Description',
+                        actions: MarkdownType.values,
+                        controller: _about,
+                      ),
                       //decoration: ThemeHelper().inputBoxDecorationShaddow(),
                     ),
-                    SizedBox(height: 30,),
+                    SizedBox(
+                      height: 30,
+                    ),
                     Container(
-                      child: TextField(
-                        controller: _price,
-                        inputFormatters: <TextInputFormatter>[
-                          CurrencyTextInputFormatter(
-                            decimalDigits: 0,
-                            symbol: 'TL'
-                          )
-                        ],
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Price',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        TextField(
+                          controller: _price,
+                          inputFormatters: <TextInputFormatter>[
+                            CurrencyTextInputFormatter(
+                                decimalDigits: 0, symbol: 'TL')
+                          ],
+                        ),
+                      ],
+                    )),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text('LOCATION INFO'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton2(
+                          hint: Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text(
+                              'Location *',
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                          iconEnabledColor: Theme.of(context).primaryColor,
+                          items: items
+                              .map((item) => DropdownMenuItem<String>(
+                                    value: item,
+                                    child: Text(
+                                      item,
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ))
+                              .toList(),
+                          value: selectedValue,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedValue = value as String;
+                            });
+                          },
+                          buttonHeight: 40,
+                          buttonWidth: width,
+                          itemHeight: 40,
+                        ),
                       ),
                     ),
-                    SizedBox(height: 30,),
+                    SizedBox(
+                      height: 30,
+                    ),
                     Container(
                       child: TextFormField(
-                        controller: _email,
-                        decoration: ThemeHelper().textInputDecoration(
-                            "E-mail address".tr, "Enter your email".tr),
+                        controller: _region,
+                        decoration: ThemeHelper()
+                            .textInputDecoration("Region".tr, "".tr),
                         keyboardType: TextInputType.emailAddress,
                       ),
                       decoration: ThemeHelper().inputBoxDecorationShaddow(),
@@ -169,10 +288,44 @@ class _UploadDataState extends State<UploadData> {
                     SizedBox(height: 20.0),
                     Container(
                       child: TextFormField(
+                        controller: _postalCode,
+                        decoration: ThemeHelper()
+                            .textInputDecoration("Postal Code".tr, "".tr),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      decoration: ThemeHelper().inputBoxDecorationShaddow(),
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                      child: TextFormField(
+                        controller: _address,
+                        decoration: ThemeHelper()
+                            .textInputDecoration("Address".tr, "".tr),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      decoration: ThemeHelper().inputBoxDecorationShaddow(),
+                    ),
+                    SizedBox(height: 30.0),
+                    Text('SELLER INFO'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: TextFormField(
                         controller: _name,
-                        obscureText: true,
-                        decoration: ThemeHelper().textInputDecoration(
-                            "Password".tr, "Enter your password".tr),
+                        decoration:
+                            ThemeHelper().textInputDecoration("Name".tr, "".tr),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      decoration: ThemeHelper().inputBoxDecorationShaddow(),
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                      child: TextFormField(
+                        controller: _phone,
+                        decoration: ThemeHelper()
+                            .textInputDecoration("Phone".tr, "".tr),
+                        keyboardType: TextInputType.emailAddress,
                       ),
                       decoration: ThemeHelper().inputBoxDecorationShaddow(),
                     ),
@@ -192,7 +345,7 @@ class _UploadDataState extends State<UploadData> {
                                       });
                                     }),
                                 Text(
-                                  "I accept all terms and conditions.".tr,
+                                  "Show phone number in post".tr,
                                   style: TextStyle(color: Colors.grey),
                                 ),
                               ],
@@ -213,6 +366,83 @@ class _UploadDataState extends State<UploadData> {
                       },
                     ),
                     SizedBox(height: 20.0),
+                    Container(
+                      child: TextFormField(
+                        controller: _email,
+                        decoration: ThemeHelper().textInputDecoration(
+                            "E-mail address".tr, "Enter your email".tr),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      decoration: ThemeHelper().inputBoxDecorationShaddow(),
+                    ),
+                    SizedBox(height: 15.0),
+                    FormField<bool>(
+                      builder: (state) {
+                        return Column(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Checkbox(
+                                    value: checkboxValue,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        checkboxValue = value!;
+                                        state.didChange(value);
+                                      });
+                                    }),
+                                Text(
+                                  "Show email adress in post".tr,
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                state.errorText ?? '',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Theme.of(context).errorColor,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                        child: Column(
+                      children: [
+                        Text(
+                          'PHOTOS',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        SizedBox(height: 20.0),
+                        Text(
+                          'A post can have at most 10 photos',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        //Flexible(child: buildGridView()),
+                        Container(
+                          height: 250,
+                          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: buildGridView(),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 240, 235, 235),
+                            borderRadius: BorderRadius.circular(5) ),
+                        ),
+                        RaisedButton(
+                            child: Text(
+                              'Upload Photos',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            onPressed: loadAssets),
+                      ],
+                    )),
+                    SizedBox(height: 40.0),
                     Container(
                       decoration: ThemeHelper().buttonBoxDecoration(context),
                       child: ElevatedButton(
